76a7111a1db4080d551b060014ae786d0f674494
Harman Singh <singh.harmanveer2003@gmail.com>
2026-03-30 02:08:57 +0530

    fix inngest v4 syntax

--- inngest/client.js
+++ inngest/client.js
@@ -1,6 +1,3 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ 
-  id: "gocart-ecommerce",
-  isDev: process.env.NODE_ENV === "development",
-});
+export const inngest = new Inngest({ id: "gocart-ecommerce" });

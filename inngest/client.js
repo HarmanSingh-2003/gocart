168a4418d3c8e824da06d6fcd5a01cf2f4511fcf
Harman Singh <singh.harmanveer2003@gmail.com>
2026-03-30 01:09:55 +0530

    fix inngest client

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "gocart-ecommerce" });
+export const inngest = new Inngest({ 
+  id: "gocart-ecommerce",
+  isDev: process.env.NODE_ENV === "development",
+});
